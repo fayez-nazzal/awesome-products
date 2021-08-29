d4b8d5dc9d6a233d0f7bc92d07606e0fa08db0a7
Fayez Nazzal <49946791+fayez-nazzal@users.noreply.github.com>
2021-08-29 08:54:29 +0300

    attempt to inject GA

--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+const injectGoogleAnalytics = () => {
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+
+  gtag("js", new Date());
+
+  gtag("config", "UA-206070895-1");
+};
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=UA-206070895-1"
+    ></script>
+    <script>{injectGoogleAnalytics()}</script>
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
