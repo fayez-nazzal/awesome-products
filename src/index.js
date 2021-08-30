23c5c3f4a6763ae5436a9be707ca6e2696ef631b
Fayez Nazzal <49946791+fayez-nazzal@users.noreply.github.com>
2021-08-30 05:04:29 +0300

    attempt to fix react router with ga

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import App from "./App";
+
+const history = createBrowserHistory();
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter>
+    <Router history={history}>
       <App />
-    </BrowserRouter>
+    </Router>
   </StrictMode>,
   rootElement
 );
+
+history.listen((location) => {
+  window.ga("set", "page", location.pathname + location.search);
+  window.ga("send", "pageview");
+});
